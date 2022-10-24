d0c2be3133ba2ad9bbb26b972e2e720995a5a6b1
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-10-24 17:38:09 +0300

    add fresh badge

--- routes/index.tsx
+++ routes/index.tsx
@@ -9,6 +9,16 @@ export default function Home() {
       <div class="p-4 mx-auto max-w-screen-md">
         <TodoList />
       </div>
+      <footer class="absolute bottom-0">
+        <a href="https://fresh.deno.dev">
+          <img
+            width="197"
+            height="37"
+            src="https://fresh.deno.dev/fresh-badge.svg"
+            alt="Made with Fresh"
+          />
+        </a>
+      </footer>
     </div>
   );
 }
